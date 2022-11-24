801114623aa704914166bccde9b9b55bb1b45b35
Nermin Skenderovic <nermin@skenderovic.se>
2022-11-24 13:34:18 +0100

    WIP hover effect

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -7,8 +7,23 @@ interface CardProps {
 }
 
 export default function Card(props: CardProps) {
+  const handleOnMouseMove = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    const target = e.currentTarget
+
+    const rect = target.getBoundingClientRect()
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+
+    target.style.setProperty('--mouse-x', x + 'px')
+    target.style.setProperty('--mouse-y', y + 'px')
+  }
+
   return (
-    <a href={props.link} className={`${styles.card} ${styles.grid_item}`}>
+    <a
+      href={props.link}
+      className={`${styles.card} ${styles.grid_item}`}
+      onMouseMove={handleOnMouseMove}
+    >
       <h2 className={styles.title}>{props.title}</h2>
       <p className={styles.description}>{props.description}</p>
       {/* &rarr; */}
